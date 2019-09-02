81d8fc5c43195a99c342d2954a54ddd89ca72c86
Aditya Mankare <adityamankare@gmail.com>
2019-09-02 01:12:13 -0500

    Update Two-Sum.js

--- Two-Sum.js
+++ Two-Sum.js
@@ -13,3 +13,25 @@ var twoSum = function (nums, target) {
     return resultArray;
 }
 console.log(twoSum([2, 7, 11, 15], 9));
+
+
+
+// --- Efficient Solution which accounts for Time Complexity and Space Complexity = O(n)
+var twoSum = function(nums, target){
+
+	var obj = {};
+	var resultArray = [];
+
+	for( let i = 0; i < nums.length; i++){
+		if(obj.hasOwnProperty(nums[i])){
+			resultArray[0] = obj[nums[i]];
+			resultArray[1] = i;
+			return resultArray;
+		}
+		else{
+			obj[target-nums[i]] = i;
+		}
+	}
+}
+
+//twoSum([2, 7, 11, 15], 26);
