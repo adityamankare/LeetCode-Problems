a74c61693b904095fec999dab42fe30d5c085f8a
Aditya Mankare <adityamankare@gmail.com>
2019-09-07 15:34:45 -0500

    Update Maximum-Subarray.js

--- Maximum-Subarray.js
+++ Maximum-Subarray.js
@@ -1,4 +1,4 @@
-//Brute Force Solution
+//Brute Force Solution//
 function maximumSubArray(arr){
 
 	let maximumSum = -Infinity;
@@ -19,4 +19,25 @@ function maximumSubArray(arr){
 console.log(maximumSubArray([2,5,11,-15]));
 
 //O(n^2) - Time Complexity
+//O(1) - Space Complexity
+
+//----------
+
+//Efficient Solution using Kadane's Algorithm//
+var maxSubArray = function(nums){
+
+	let globalMaximum = nums[0];
+	let currentMaximum = nums[0];
+
+	for(let i=1; i<(nums.length-1); i++){
+		currentMaximum = Math.max(currentMaximum, currentMaximum + nums[i]);
+		if(globalMaximum < currentMaximum){
+			globalMaximum = currentMaximum;
+		}
+	}
+	return globalMaximum
+}
+
+console.log(maxSubArray([8,9,-5,20]));
+//O(n) - Time Complexity
 //O(1) - Space Complexity
