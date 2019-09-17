e76c91643bea2de453bddb596db79184473c8479
Aditya Mankare <adityamankare@gmail.com>
2019-09-17 13:16:28 -0500

    Create LC#13-romanToInt.py

---
+++ LC#13-romanToInt.py
@@ -0,0 +1,20 @@
+class Solution(object):
+  def romanToInt(self, s):
+    
+    singles = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+    doubles = {'CM' : 900, 'CD' : 400, 'XC' : 90, 'XL' : 40, 'IX' :9, 'IV' : 4}
+
+    result = 0
+    i = 0
+
+    while i < len(s):
+      if i < len(s)-1 and s[i:i+2] in doubles:
+        result += doubles[s[i:i+2]]
+        i += 2
+      else:
+        result += singles[s[i]]
+        i += 1
+    return result
+
+#O(n) time complexity
+#O(1) space complexity
