b2ceaff20ec4be6280dd3d8440032e8af6a5196e
Aditya Mankare <adityamankare@gmail.com>
2019-10-06 11:21:10 -0500

    Create LC#344-reverseString.py

---
+++ LC#344-reverseString.py
@@ -0,0 +1,13 @@
+class Solution:
+  def reverseString(self,s):
+    s.reverse()
+    return s
+#O(n) time
+#O(n) space
+
+#Alternate Solution
+class Solution2:
+  def reverseString2(self,s2):
+    return s2[::-1]
+#O(n) time
+#O(n) space
