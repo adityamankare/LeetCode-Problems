d385337c1db6a09b77126fa2bb0654683807cec0
Aditya Mankare <adityamankare@gmail.com>
2019-10-08 13:56:41 -0500

    Create LC#125-validPalindrome.py

---
+++ LC#125-validPalindrome.py
@@ -0,0 +1,16 @@
+import string
+class Solution:
+  def isPalindrome(self,s):
+    allowed = set(string.ascii_lowercase + string.digits)
+    s = [c for c in s.lower() if c in allowed]
+    i = 0
+    j = len(s)-1
+    while i < j:
+      if s[i] != s[j]:
+        return False
+      i += 1
+      j -= 1
+    return True
+    
+#O(n) time
+#O(n) space
